cc3d8246cf5d692e99fd8f878243b28698b12be8
yukimianyan <cjh090406@163.com>
2024-02-14 23:01:58 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # template-poly
-OI 中的多项式模板，使用 vector&lt;modint> 实现
+The repository is still a **work in progress**.
+
+OI 中的多项式模板，使用 `std::vector<modint<998244353>>` 实现。目前已实现：
+- modint
+- 单点求值
+- NTT
+- 多项式加法、减法、数乘
+- 多项式乘法
+- 多项式乘法逆
+- 分式 $f(x)/g(x)$ 的第 $n$ 项
+- 常系数齐次线性递推
+- 求最短递推式的 BM 算法
+- $O(n^2)$ 的拉格朗日插值
+
+目前未实现：
+- 多项式除法、取模
+- 多项式对数函数
+- 多项式指数函数
+- 多项式牛顿迭代需要一个通用的接口，没想好怎么写。
+- ~~文档~~
+
+```sh
+g++ poly.cpp -o main -std=c++14
+```
---
+++ poly.cpp
@@ -0,0 +1,295 @@
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef LOCAL
+#define debug(...) fprintf(stderr, ##__VA_ARGS__)
+#else
+#define endl "\n"
+#define debug(...) void(0)
+#endif
+typedef long long LL;
+template <unsigned umod>
+struct modint {
+  static constexpr int mod = umod;
+  unsigned v;
+  modint() : v(0) {}
+  template <class T, enable_if_t<is_integral<T>::value, int> = 0>
+  modint(T x) {
+    x %= mod;
+    if (x < 0) x += mod;
+    v = x;
+  }
+  modint operator+() const { return *this; }
+  modint operator-() const { return modint() - *this; }
+  friend int raw(const modint& self) { return self.v; }
+  friend istream& operator>>(istream& is, modint& self) {
+    string str;
+    is >> str;
+    self = 0;
+    int flag = 1;
+    for (char ch : str) {
+      if (ch == '-')
+        flag = -1;
+      else
+        assert(isdigit(ch)), self = self * 10 + ch - '0';
+    }
+    self *= flag;
+    return is;
+  }
+  friend ostream& operator<<(ostream& os, const modint& self) {
+    return os << raw(self);
+  }
+  modint& operator+=(const modint& rhs) {
+    v += rhs.v;
+    if (v >= umod) v -= umod;
+    return *this;
+  }
+  modint& operator-=(const modint& rhs) {
+    v -= rhs.v;
+    if (v >= umod) v += umod;
+    return *this;
+  }
+  modint& operator*=(const modint& rhs) {
+    v = static_cast<unsigned>(1ull * v * rhs.v % umod);
+    return *this;
+  }
+  modint& operator/=(const modint& rhs) {
+    assert(rhs.v);
+    return *this *= qpow(rhs, mod - 2);
+  }
+  template <class T>
+  friend modint qpow(modint a, T b) {
+    modint r = 1;
+    for (; b; b >>= 1, a *= a)
+      if (b & 1) r *= a;
+    return r;
+  }
+  friend modint operator+(modint lhs, const modint& rhs) { return lhs += rhs; }
+  friend modint operator-(modint lhs, const modint& rhs) { return lhs -= rhs; }
+  friend modint operator*(modint lhs, const modint& rhs) { return lhs *= rhs; }
+  friend modint operator/(modint lhs, const modint& rhs) { return lhs /= rhs; }
+  friend bool operator==(const modint& lhs, const modint& rhs) {
+    return lhs.v == rhs.v;
+  }
+  friend bool operator!=(const modint& lhs, const modint& rhs) {
+    return lhs.v != rhs.v;
+  }
+};
+typedef modint<998244353> mint;
+int glim(const int& x) { return 1 << (32 - __builtin_clz(x - 1)); }
+int bitctz(const int& x) { return __builtin_ctz(x); }
+struct poly : vector<mint> {
+  poly() {}
+  explicit poly(int n) : vector<mint>(n) {}
+  poly(const vector<mint>& vec) : vector<mint>(vec) {}
+  poly(initializer_list<mint> il) : vector<mint>(il) {}
+  mint operator()(const mint& x) const;
+  void ntt(const int& op);
+  poly getInv(int len) const;
+  poly cut(int lim) const;
+};
+void print(const poly& a) {
+  for (int i = 0; i < a.size(); i++) debug("%d, ", raw(a[i]));
+  debug("\n");
+}
+mint poly::operator()(const mint& x) const {
+  const vector<mint>& a = *this;
+  mint res = 0;
+  for (int i = (int)a.size() - 1; i >= 0; i--) {
+    res = res * x + a[i];
+  }
+  return res;
+}
+poly cut(poly a, int lim) {
+  a.resize(lim);
+  return a;
+}
+poly poly::cut(int lim) const {
+  return ::cut(*this, lim);
+}
+void poly::ntt(const int& op) {
+  static bool wns_flag = false;
+  static vector<mint> wns;
+  if (!wns_flag) {
+    wns_flag = true;
+    for (int j = 1; j <= 23; j++) {
+      wns.push_back(qpow(mint(3), raw(mint(-1)) >> j));
+    }
+  }
+  vector<mint>& a = *this;
+  int n = a.size();
+  for (int i = 1, r = 0; i < n; i++) {
+    r ^= n - (1 << (bitctz(n) - bitctz(i) - 1));
+    if (i < r) std::swap(a[i], a[r]);
+  }
+  vector<mint> w(n);
+  for (int k = 1, len = 2; len <= n; k <<= 1, len <<= 1) {
+    mint wn = wns[bitctz(k)];
+    for (int i = raw(w[0] = 1); i < k; i++) w[i] = w[i - 1] * wn;
+    for (int i = 0; i < n; i += len) {
+      for (int j = 0; j < k; j++) {
+        mint x = a[i + j], y = a[i + j + k] * w[j];
+        a[i + j] = x + y, a[i + j + k] = x - y;
+      }
+    }
+  }
+  if (op == -1) {
+    mint iz = mint(1) / n;
+    for (int i = 0; i < n; i++) a[i] *= iz;
+    reverse(a.begin() + 1, a.end());
+  }
+}
+poly concalc(int n, vector<poly> vec, const function<mint(vector<mint>)>& func) {
+  int lim = glim(n);
+  int m = vec.size();
+  for (auto& f : vec) f.resize(lim), f.ntt(1);
+  vector<mint> tmp;
+  poly ret(lim);
+  for (int i = 0; i < lim; i++) {
+    for (int j = 0; j < m; j++) tmp[j] = vec[j][i];
+    ret[i] = func(tmp);
+  }
+  ret.ntt(-1);
+  return ret;
+}
+poly getInv(const poly& a, int lim) {
+  poly b{1 / a[0]};
+  for (int len = 2; len <= glim(lim); len <<= 1) {
+    poly c = vector<mint>(a.begin(), a.begin() + min(len, (int)a.size()));
+    b = concalc(len << 1, {b, c}, [](vector<mint> vec) { return vec[0] * (2 - vec[0] * vec[1]); }).cut(len);
+  }
+  return b.cut(lim);
+}
+poly poly::getInv(int lim) const { return ::getInv(*this, lim); }
+poly operator+=(poly& a, const poly& b) {
+  if (a.size() < b.size()) a.resize(b.size());
+  for (int i = 0; i < b.size(); i++) a[i] += b[i];
+  return a;
+}
+poly operator-=(poly& a, const poly& b) {
+  if (a.size() < b.size()) a.resize(b.size());
+  for (int i = 0; i < b.size(); i++) a[i] -= b[i];
+  return a;
+}
+poly operator*=(poly& a, const mint& k) {
+  for (int i = 0; i < a.size(); i++) a[i] *= k;
+  return a;
+}
+poly operator<<=(poly& a, const int& k) {
+  // mnltiple by x^k
+  a.insert(a.begin(), k, 0);
+  return a;
+}
+poly operator>>=(poly& a, const int& k) {
+  // divide by x^k
+  a.erase(a.begin(), a.begin() + min(k, (int)a.size()));
+  return a;
+}
+poly operator*(const poly& a, const poly& b) {
+  if (a.empty() || b.empty()) return {};
+  int rlen = a.size() + b.size() - 1;
+  int len = glim(rlen);
+  if (1ll * a.size() * b.size() <= len * bitctz(len)) {
+    poly ret(rlen);
+    for (int i = 0; i < a.size(); i++) {
+      for (int j = 0; j < b.size(); j++) {
+        ret[i + j] += a[i] * b[j];
+      }
+    }
+    return ret;
+  } else {
+    return concalc(len, {a, b}, [](vector<mint> vec) { return vec[0] * vec[1]; }).cut(rlen);
+  }
+}
+poly operator*=(poly& a, const poly& b) { return a = a * b; }
+poly operator+(poly a, const poly& b) { return a += b; }
+poly operator-(poly a, const poly& b) { return a -= b; }
+poly operator*(poly a, const mint& k) { return a *= k; }
+poly operator*(const mint& k, poly a) { return a *= k; }
+poly operator<<(poly a, const int& k) { return a <<= k; }
+poly operator>>(poly a, const int& k) { return a >>= k; }
+template <class T>
+mint divide_at(poly f, poly g, T n) {
+  for (; n; n >>= 1) {
+    poly r = g;
+    for (int i = 1; i < r.size(); i += 2) r[i] *= -1;
+    f *= r;
+    g *= r;
+    for (int i = n & 1; i < f.size(); i += 2) f[i >> 1] = f[i];
+    f.resize((f.size() + 1) >> 1);
+    for (int i = 0; i < g.size(); i += 2) g[i >> 1] = g[i];
+    g.resize((g.size() + 1) >> 1);
+  }
+  return f.empty() ? 0 : f[0] / g[0];
+}
+template <class T>
+mint linear_rec(poly a, poly f, T n) {
+  // a[n] = sum_i f[i] * a[n - i]
+  a.resize(f.size() - 1);
+  f = poly{1} - f;
+  poly g = a * f;
+  g.resize(a.size());
+  return divide_at(g, f, n);
+}
+poly BM(poly a) {
+  poly ans, lst;
+  int w = 0;
+  mint delta = 0;
+  for (int i = 0; i < a.size(); i++) {
+    mint tmp = -a[i];
+    for (int j = 0; j < ans.size(); j++) tmp += ans[j] * a[i - j - 1];
+    if (tmp == 0) continue;
+    if (ans.empty()) {
+      w = i;
+      delta = tmp;
+      ans = vector<mint>(i + 1, 0);
+    } else {
+      auto now = ans;
+      mint mul = -tmp / delta;
+      if (ans.size() < lst.size() + i - w) ans.resize(lst.size() + i - w);
+      ans[i - w - 1] -= mul;
+      for (int j = 0; j < lst.size(); j++) ans[i - w + j] += lst[j] * mul;
+      if (now.size() <= lst.size() + i - w) {
+        w = i;
+        lst = now;
+        delta = tmp;
+      }
+    }
+  }
+  return ans << 1;
+}
+poly lagrange(const vector<pair<mint, mint>>& a) {
+  poly ans(a.size()), product{1};
+  for (int i = 0; i < a.size(); i++) {
+    product *= poly{-a[i].first, 1};
+  }
+  auto divide2 = [&](poly a, mint b) {
+    poly res(a.size() - 1);
+    for (int i = (int)a.size() - 1; i >= 1; i--) {
+      res[i - 1] = a[i];
+      a[i - 1] -= a[i] * b;
+    }
+    return res;
+  };
+  for (int i = 0; i < a.size(); i++) {
+    mint denos = 1;
+    for (int j = 0; j < a.size(); j++) {
+      if (i != j) denos *= a[i].first - a[j].first;
+    }
+    poly numes = divide2(product, -a[i].first);
+    ans += a[i].second / denos * numes;
+  }
+  return ans;
+}
+int main() {
+#ifndef LOCAL
+  cin.tie(nullptr)->sync_with_stdio(false);
+#endif
+  int n, m;
+  cin >> n >> m;
+  vector<mint> a(n + 1), b(m + 1);
+  for (auto& x : a) cin >> x;
+  for (auto& x : b) cin >> x;
+  for (auto x : a* b) cout << x << " ";
+  cout << endl;
+  return 0;
+}
