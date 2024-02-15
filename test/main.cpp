f0178f7ffb24a6dbd1943fdd8b25262ecbb390a6
yukimianyan <cjh090406@163.com>
2024-02-15 16:13:24 +0800

    feat: tester

--- test/main.cpp
+++ test/main.cpp
@@ -336,3 +336,19 @@ poly lagrange(const vector<pair<mint, mint>>& a) {
   return ans;
 }
 
+
+void TestGetLn() {
+  int n;
+  cin >> n;
+  poly a(n);
+  for (auto& x : a) cin >> x;
+  for (auto x : a.getLn(n)) cout << x << " ";
+  cout << endl;
+}
+int main() {
+#ifndef LOCAL
+  cin.tie(nullptr)->sync_with_stdio(false);
+#endif
+  TestGetLn();
+  return 0;
+}
