9a249078d4f1b8c4d799592f63af5780a47633b9
yukimianyan <cjh090406@163.com>
2024-05-20 16:52:18 +0800

    refactor: modint.hpp ok

---
+++ poly/header.h
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+#ifdef LOCAL
+#define debug(...) fprintf(stderr, ##__VA_ARGS__)
+#else
+#define endl "\n"
+#define debug(...) void(0)
+#endif
+typedef long long LL;
--- test/test000/main.cpp
+++ test/test000/main.cpp
@@ -399,3 +399,16 @@ poly lagrange(const vector<pair<mint, mint>>& a) {
   }
   return ans;
 }
+int main() {
+#ifndef LOCAL
+  cin.tie(nullptr)->sync_with_stdio(false);
+#endif  
+  int n, k;
+  cin >> n >> k;
+  vector<pair<mint, mint>> a(n);
+  for (auto& elem : a) cin >> elem.first >> elem.second;
+  cout << lagrange(a)(k) << endl;
+  cerr << lagrange(a) << endl;
+  return 0;
+}
+
