c3f833253090b9f9113f5c4ede4b022c4fa1ec9f
yukimianyan <cjh090406@163.com>
2024-10-14 10:30:39 +0800

    fix(getinv, getln)：修改接口

--- poly/getinv.hpp
+++ poly/getinv.hpp
@@ -7,9 +7,9 @@ vector<mint> getInv(const vector<mint>& a, int lim) {
   for (int len = 2; len <= glim(lim); len <<= 1) {
     vector<mint> c(a.begin(), a.begin() + min(len, (int)a.size()));
     auto difb = dif(b, len);
-    c = dit(dif(c, len) * difb, len);
+    c = dit(dif(c, len) * difb);
     for (int i = 0; i < len >> 1; i++) c[i] = 0;
-    c = dit(dif(c, len) * difb, len);
+    c = dit(dif(c, len) * difb);
     b.resize(len);
     for (int i = len >> 1; i < len; i++) b[i] = -c[i];
   }
--- poly/getln.hpp
+++ poly/getln.hpp
@@ -4,7 +4,7 @@
 #include "poly/getinv.hpp"
 #include "poly/header.h"
 #include "poly/multiple.hpp"
-template <class mint, int g = 3>
+template <class mint>
 vector<mint> getLn(const vector<mint>& a, int lim) {
   assert(a[0] == 1);
   return cut(getInt(getDev(a) * getInv(a, lim)), lim);
