007bcd0043129fb1564661c4cf09a794227b2d29
yukimianyan <cjh090406@163.com>
2024-10-14 09:17:00 +0800

    perf(ntt.hpp): 引入变量模板

--- src/ntt.hpp
+++ src/ntt.hpp
@@ -2,20 +2,23 @@
 #include "base.hpp"
 #include "header.h"
 #include "internal_math.hpp"
+template <class mint> vector<mint> wt;
 template <class mint>
-void init(vector<mint>& w, int n) {
+vector<mint>& init(int n) {
   static constexpr int g = poly_internal::pmtroot(mint::mod);
+  auto& w = wt<mint>;
+  if (w.empty()) w = {1};
   while ((int)w.size() < n) {
     int m = w.size();
     mint wn = qpow(mint(g), (mint::mod - 1) / m >> 2);
     w.resize(m << 1);
     for (int i = m; i < m << 1; i++) w[i] = wn * w[i ^ m];
   }
+  return w;
 }
 template <class mint>
 valarray<mint> dif(const vector<mint>& src, int n) {
-  static vector<mint> w{1};
-  init(w, n);
+  auto& w = init<mint>(n);
   valarray<mint> a(0, n);
   copy(src.begin(), src.end(), begin(a));
   for (int len = n, k = n >> 1; k >= 1; len = k, k >>= 1) {
@@ -33,8 +36,7 @@ auto dit(T&& _src, int m) {
   using mint = typename T::value_type;
   valarray<mint> src = _src;
   int n = (int)src.size();
-  static vector<mint> w{1};
-  init(w, n);
+  auto& w = init<mint>(n);
   vector<mint> a(begin(src), end(src));
   for (int k = 1, len = 2; len <= n; k = len, len <<= 1) {
     for (int i = 0, t = 0; i < n; i += len, t++) {
